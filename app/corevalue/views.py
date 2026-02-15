bc56b9e0e75d2685758248b030fb1421e7d9e8d1
kuzowebsite <m.batireedui@ider.edu.mn>
2026-02-16 04:10:38 +0800

    fix: manually delete translations before CoreValue to avoid FK constraint (DO_NOTHING)

--- app/corevalue/views.py
+++ app/corevalue/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
-from app.models.models import CoreValue
+from rest_framework.response import Response
+from rest_framework import status
+from app.models.models import CoreValue, CoreValueTitleTranslations, CoreValueDescTranslations
 from app.corevalue.serializers import CoreValueReadSerializer, CoreValueWriteSerializer
 
 
@@ -15,3 +17,12 @@ class CoreValueViewSet(viewsets.ModelViewSet):
         if self.action in ['list', 'retrieve']:
             return CoreValueReadSerializer
         return CoreValueWriteSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        """Delete core value and its translations manually (DO_NOTHING FK)."""
+        instance = self.get_object()
+        # Delete related translations first to avoid FK constraint errors
+        CoreValueTitleTranslations.objects.filter(corevalue=instance).delete()
+        CoreValueDescTranslations.objects.filter(corevalue=instance).delete()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
